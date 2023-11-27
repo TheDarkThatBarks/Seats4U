d2134c983a591e4964f5f8e3ef8a9e0249986a81
TheDarkThatBarks <125921991+TheDarkThatBarks@users.noreply.github.com>
2023-11-27 14:31:12 -0500

    Merging createVenue into main

---
+++ db_access/nodejs/db_access.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // credentials to the DB are here. HIDDEN and not exposed outside
+    // NOTE: YOU SHOULD REPLACE WITH YOUR OWN NATURALLY. These are the ones
+    // that you set up 
+    config: {
+        "host": "seats4udb.c2lgduavgifp.us-east-2.rds.amazonaws.com",
+        "user": "seats4uAdmin",
+        "password": "seats4u:pass",
+        "database": "seats4u"
+    }
+};
