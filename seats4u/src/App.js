00a9b3a3995f2f228ab12360c712a454b1ebc3a6
Collin Shields <36793080+TheMusesMagic@users.noreply.github.com>
2023-11-27 19:38:24 -0500

    Added some input fields

--- seats4u/src/App.js
+++ seats4u/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React from 'react'
+import { createVenue } from './controller/CreateVenue';
+
 function App() {
+  const [redraw, forceRedraw] = React.useState(0);
+
+  React.useEffect(()=>{
+  }, [redraw]);
+
+  // this function requests the redraw, and can be passed as an argument to other functions
+  const requestRedraw = () => {
+    forceRedraw(redraw+1)
+  }
+
+  const createVenueHandler = (e) => {
+    createVenue(requestRedraw)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="Seats4U">
+      <h1>inputs</h1>
+      name: <input id="venue-name"/>
+      side left rows: <input id="venue-sideLeft-rows"/>
+      side left cols: <input id="venue-sideLeft-cols"/>
+      center rows: <input id="venue-center-rows"/>
+      center cols: <input id="venue-center-cols"/>
+      side right rows: <input id="venue-sideRight-rows"/>
+      side right cols: <input id="venue-sideRight-cols"/>
+      password: <input id="venue-password"/>
+      <button onClick={createVenueHandler}>Create</button>
+  </div>
   );
 }
 
