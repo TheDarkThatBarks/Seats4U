a5cf5e4c10a3669aafd73677e8559718e4fae299
Oliner, Steven Michel (786349968) <smoliner@wpi.edu>
2023-11-28 14:54:32 -0500

    Started Show-related files

---
+++ seats4u/src/pages/CreateShowPage.jsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import React from 'react'
+import { createShow } from '../controller/CreateShow';
+
+export const CreateShowPage = () => {
+    const [redraw, forceRedraw] = React.useState(0);
+
+    React.useEffect(()=>{}, [redraw]);
+
+    const createShowHandler = (e) => {
+        createShow(requestRedraw)
+    }
+
+    const requestRedraw = () => {
+        forceRedraw(redraw+1)
+    }
+
+    return (
+        <div className="Seats4U">
+            <h1>inputs</h1>
+                name: <input id="show-name"/>
+                price: <input id="show-price"/>
+                date: <input id="show-date"/>
+                time: <input id="show-time"/>
+            <button onClick={createShowHandler}>Create</button>
+        </div>
+    );
+}
