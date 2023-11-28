802c2aa055f76facd551c57e472a225f54dfcf3d
Collin Shields <36793080+TheMusesMagic@users.noreply.github.com>
2023-11-28 14:07:31 -0500

    Added routing

--- seats4u/src/App.js
+++ seats4u/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 
 import React from 'react'
-import { createVenue } from './controller/CreateVenue';
+import { Routes, Route } from 'react-router-dom';
+
+import { CreateVenuePage } from './pages/CreateVenuePage';
 
 function App() {
   const [redraw, forceRedraw] = React.useState(0);
@@ -14,23 +16,12 @@ function App() {
     forceRedraw(redraw+1)
   }
 
-  const createVenueHandler = (e) => {
-    createVenue(requestRedraw)
-  }
-
   return (
-    <div className="Seats4U">
-      <h1>inputs</h1>
-      name: <input id="venue-name"/>
-      side left rows: <input id="venue-sideLeft-rows"/>
-      side left cols: <input id="venue-sideLeft-columns"/>
-      center rows: <input id="venue-center-rows"/>
-      center cols: <input id="venue-center-columns"/>
-      side right rows: <input id="venue-sideRight-rows"/>
-      side right cols: <input id="venue-sideRight-columns"/>
-      password: <input id="venue-password"/>
-      <button onClick={createVenueHandler}>Create</button>
-  </div>
+    <div>
+      <Routes>
+				<Route path="/" element={<CreateVenuePage />} />
+			</Routes>
+    </div>
   );
 }
 
