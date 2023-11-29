ce1f494a7e562efd2ca4c92d7d48a0d577506ba0
Collin Shields <36793080+TheMusesMagic@users.noreply.github.com>
2023-11-29 15:36:12 -0500

    Listing Shows

--- seats4u/src/Pages/Home.jsx
+++ seats4u/src/Pages/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { listShows } from '../controller/ListShows';
 
 export const Home = () => {
-	const [redraw] = React.useState(0);
+	const [redraw, forceRedraw] = React.useState(0);
+
+	React.useEffect(()=>{
+		listShows()
+	}, [redraw]);
+
+	const requestRedraw = () => {
+		forceRedraw(redraw+1)
+	}
+	
 
 	const navigate = useNavigate();
 
@@ -10,6 +20,7 @@ export const Home = () => {
 		<div className="Home">
 			<button onClick={() => navigate('venuemanager')}>Venue Manager Page</button>
 			<button onClick={() => navigate('admin')}>Admin Page</button>
+			<div id="show-list"></div>
 		</div>
 	);
 };
---
+++ seats4u/src/controller/ListShows.js
@@ -0,0 +1,23 @@
+import { get } from "./API"
+
+export function listShows() {
+    // this sends the ACTUAL POST and retrieves the answer.
+    get('/consumer/listShows')
+        .then(function (response) {
+
+            let str = ''
+            for (let s of response.shows) {
+                str += "Show: " + s.name + " | Venue Name: " + s.venueName + '<br>';
+            }
+
+            // insert HTML in the <div> with 
+            // constant-list
+            let cd = document.getElementById('show-list')
+            cd.innerHTML = str
+
+        })
+        .catch(function (error) {
+            // not much to do
+            console.log(error)
+        })
+    }
