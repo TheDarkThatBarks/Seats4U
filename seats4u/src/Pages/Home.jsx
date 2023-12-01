295054e21d3ede4940ba5cb3f1d0450509e82261
TheDarkThatBarks <125921991+TheDarkThatBarks@users.noreply.github.com>
2023-12-01 15:34:47 -0500

    Lot of changes on this one

--- seats4u/src/Pages/Home.jsx
+++ seats4u/src/Pages/Home.jsx
@@ -1,26 +1,54 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { listShows } from '../controller/ListShows';
+import { post } from '../controller/API';
 
 export const Home = () => {
-	const [redraw, forceRedraw] = React.useState(0);
+    const [redraw, forceRedraw] = React.useState(0);
 
-	React.useEffect(()=>{
-		listShows()
-	}, [redraw]);
+    React.useEffect(() => {
+        listShows();
+    }, [redraw]);
 
-	const requestRedraw = () => {
-		forceRedraw(redraw+1)
-	}
-	
+    const requestRedraw = () => {
+        forceRedraw(redraw + 1);
+    };
 
-	const navigate = useNavigate();
+    const validateVenueManager = async () => {
+        let venueName = document.getElementById("venue-name");
+        let venuePassword = document.getElementById("venue-password");
 
-	return (
-		<div className="Home">
-			<button onClick={() => navigate('venuemanager')}>Venue Manager Page</button>
-			<button onClick={() => navigate('admin')}>Admin Page</button>
-			<div id="show-list"></div>
-		</div>
-	);
+        let data = {
+            'venueName': venueName.value,
+            'venuePassword': venuePassword.value
+        };
+
+        const handler = (json) => {
+            console.log(json);
+            if (json.statusCode === 200) {
+                console.log("TRUE");
+                document.getElementById("data-venue-name").value = json.venue.name;
+                document.getElementById("data-venue-id").value = json.venue.venueID;
+                document.getElementById("data-venue-password").value = json.venue.password;
+                navigate('venuemanager');
+            } else {
+                console.log("FALSE");
+            }
+        };
+
+        post('/venue/login', data, handler);
+    };
+
+    const navigate = useNavigate();
+
+    return (
+        <div className="Home">
+            <button onClick={() => navigate('createvenue')}>Create Venue</button>
+            Venue Name: <input id="venue-name" />
+            Venue Password: <input id="venue-password" />
+            <button onClick={validateVenueManager}>Venue Manager Page</button>
+            <button onClick={() => navigate('admin')}>Admin Page</button>
+            <div id="show-list"></div>
+        </div>
+    );
 };
