5d29c2db9ef3d45bfc869fd1bdd3fa9c6e23c515
Oliner, Steven Michel (786349968) <smoliner@wpi.edu>
2023-12-05 13:19:26 -0500

    listShowsAdmin created

---
+++ Lambda Functions/listShowsAdmin/index.js
@@ -0,0 +1,33 @@
+const mysql = require('mysql');
+const db_access = require('/opt/nodejs/db_access');
+
+exports.handler = async (event) => {
+
+    // get credentials from the db_access layer (loaded separately via AWS console)
+    var pool = mysql.createPool({
+        host: db_access.config.host,
+        user: db_access.config.user,
+        password: db_access.config.password,
+        database: db_access.config.database
+    });
+
+    let showList = () => {
+        return new Promise((resolve, reject) => {
+            pool.query("SELECT * FROM Shows", [], (error, rows) => {
+                if (error)
+                    return reject(error);
+                return resolve(rows);
+            });
+        });
+    };
+
+    let response = undefined;
+
+    response = {
+        statusCode: 200,
+        shows: await showList()
+    };
+
+    pool.end();
+    return response;
+};
