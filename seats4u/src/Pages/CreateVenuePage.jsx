f09e29e901a9d24f7a3cbf70ed9866aa2cf504a3
Collin Shields <36793080+TheMusesMagic@users.noreply.github.com>
2023-12-06 13:47:59 -0500

    UI update to make things more clear

--- seats4u/src/Pages/CreateVenuePage.jsx
+++ seats4u/src/Pages/CreateVenuePage.jsx
@@ -21,19 +21,25 @@ export const CreateVenuePage = () => {
         <div className="Seats4U">
             <h1>Enter Information Here</h1>
             name: <input id="venue-name"/>
+            <br></br>
             side left rows: <input id="venue-sideLeft-rows"/>
+            <br></br>
             side left cols: <input id="venue-sideLeft-columns"/>
+            <br></br>
             center rows: <input id="venue-center-rows"/>
+            <br></br>
             center cols: <input id="venue-center-columns"/>
+            <br></br>
             side right rows: <input id="venue-sideRight-rows"/>
+            <br></br>
             side right cols: <input id="venue-sideRight-columns"/>
+            <br></br>
             password: <input id="venue-password"/>
-
+            <br></br>
             <button onClick={createVenueHandler}>Create</button>
-
+            <button onClick={() => navigate(-1)}>Back to Home</button>
+            <h1>Server Response</h1>
             result: <input id="db-response" readOnly/>
-
-            <button onClick={() => navigate(-1)}>Back</button>
         </div>
     );
 }
